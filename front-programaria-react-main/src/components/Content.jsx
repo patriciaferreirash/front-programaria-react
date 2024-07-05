dd6e34f5e52eaa7cfb797cea4a2e649b3a92e881
Patrícia Ester Ferreira Rodrigues <100604597+patriciaferreirash@users.noreply.github.com>
2024-07-05 16:52:56 -0300

    atualização do link

--- front-programaria-react-main/src/components/Content.jsx
+++ front-programaria-react-main/src/components/Content.jsx
@@ -14,7 +14,7 @@ export function Content() {
   const [citacao, setCitacao] = useState('')
   const [imagem, setImagem] = useState('')
   const [success, setSuccess] = useState(false)
-  const baseURL = 'https://back-end-keil.onrender.com'
+  const baseURL = 'https://back-end-keil.onrender.com/mulheres'
 
   useEffect(() => {
     async function getData() {
